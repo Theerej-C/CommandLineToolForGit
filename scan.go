0b27afd09b19f158eb000a0931dfef660e2a956d
Theerej <theerej@gmail.com>
2024-05-31 18:11:29 +0530

    The project complete

--- scan.go
+++ scan.go
@@ -4,121 +4,149 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/user"
 	"strings"
 )
 
-func scan(folder string){
-	fmt.Printf("Found folders:\n\n")
-	repo := recursiveScanFolder(folder)
-	filePath := getDotFilePath()
-	addNewSliceElementsTofile(filePath,repo)
-	fmt.Printf(("\n\nSuccessfully added\n\n"))
-}
-func recursiveScanFolder(folder string) []string{
-	return scanGitFolder(make([]string,0),folder)
-}
-func scanGitFolder(folders []string,folder string) []string{
-	folder  = strings.TrimSuffix(folder,"/")
-	f,err := os.Open(folder)
-	if err != nil{
+// getDotFilePath returns the dot file for the repos list.
+// Creates it and the enclosing folder if it does not exist.
+func getDotFilePath() string {
+	usr, err := user.Current()
+	if err != nil {
 		log.Fatal(err)
 	}
-	files, err := f.Readdir(-1);
-	f.Close()
-	if err != nil{
-		log.Fatal(err)
-	}
-	var path string
-	for _,file := range files{
-		if file.IsDir(){
-			path = folder + "/" + file.Name()
-			if file.Name() == ".git"{
-				path = strings.TrimSuffix(path,"/.git")
-				fmt.Println(path)
-				folders = append(folders, path)
-				continue
-			}
-			if file.Name() == "vendor" || file.Name() == "node_modules"{
-				continue
-			}
-			folders = scanGitFolder(folders,path)
-		}
-	}
-	return folders
-}
 
-func getDotFilePath() string{
-	usr,err := user.Current()
-	if err != nil{
-		log.Fatal(err)
-	}
 	dotFile := usr.HomeDir + "/.gogitlocalstats"
+
 	return dotFile
 }
 
-func addNewSliceElementsTofile(filePath string, repo []string) {
-	existingRepos := parseFileLinesToSlice(filePath)
-	repos := joinSlices(repo,existingRepos)
-	dumpStringSliceToFIle(repos,filePath)
-}
-
-func dumpStringSliceToFIle(repos []string, filePath string) {
-	content := strings.Join(repos,"\n")
-	os.WriteFile(filePath,[]byte(content),0755)
-}
-
-func joinSlices(new []string, existingRepos []string) []string{
-	for _,val := range new{
-		if !sliceContains(existingRepos,val){
-			existingRepos = append(existingRepos, val)
+// openFile opens the file located at `filePath`. Creates it if not existing.
+func openFile(filePath string) *os.File {
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_RDWR, 0755)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// file does not exist
+			_, err = os.Create(filePath)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			// other error
+			panic(err)
 		}
 	}
-	return existingRepos
+
+	return f
 }
 
-func sliceContains(existingRepos []string, value string) bool{
-	for _,val := range existingRepos{
-		if val == value{
+// parseFileLinesToSlice given a file path string, gets the content
+// of each line and parses it to a slice of strings.
+func parseFileLinesToSlice(filePath string) []string {
+	f := openFile(filePath)
+	defer f.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		if err != io.EOF {
+			panic(err)
+		}
+	}
+
+	return lines
+}
+
+// sliceContains returns true if `slice` contains `value`
+func sliceContains(slice []string, value string) bool {
+	for _, v := range slice {
+		if v == value {
 			return true
 		}
 	}
 	return false
 }
 
-func parseFileLinesToSlice(filePath string) []string{
-	f:= openFile(filePath)
-	defer f.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan(){
-		lines= append(lines, scanner.Text())
-	}
-	if err := scanner.Err() ; err != nil{
-		if err != io.EOF{
-			panic(err)
+// joinSlices adds the element of the `new` slice
+// into the `existing` slice, only if not already there
+func joinSlices(new []string, existing []string) []string {
+	for _, i := range new {
+		if !sliceContains(existing, i) {
+			existing = append(existing, i)
 		}
 	}
-	return lines
+	return existing
 }
 
-func openFile(filePath string) *os.File{
-	f,err := os.OpenFile(filePath,os.O_APPEND|os.O_RDWR,0755)
-	if err != nil{
-		if os.IsNotExist(err){
-			_,err = os.Create(filePath)
-			if err != nil{
-				panic(err)
+// dumpStringsSliceToFile writes content to the file in path `filePath` (overwriting existing content)
+func dumpStringsSliceToFile(repos []string, filePath string) {
+	content := strings.Join(repos, "\n")
+	ioutil.WriteFile(filePath, []byte(content), 0755)
+}
+
+// addNewSliceElementsToFile given a slice of strings representing paths, stores them
+// to the filesystem
+func addNewSliceElementsToFile(filePath string, newRepos []string) {
+	existingRepos := parseFileLinesToSlice(filePath)
+	repos := joinSlices(newRepos, existingRepos)
+	dumpStringsSliceToFile(repos, filePath)
+}
+
+// recursiveScanFolder starts the recursive search of git repositories
+// living in the `folder` subtree
+func recursiveScanFolder(folder string) []string {
+	return scanGitFolders(make([]string, 0), folder)
+}
+
+// scan scans a new folder for Git repositories
+func scan(folder string) {
+	fmt.Printf("Found folders:\n\n")
+	repositories := recursiveScanFolder(folder)
+	filePath := getDotFilePath()
+	addNewSliceElementsToFile(filePath, repositories)
+	fmt.Printf("\n\nSuccessfully added\n\n")
+}
+
+// scanGitFolders returns a list of subfolders of `folder` ending with `.git`.
+// Returns the base folder of the repo, the .git folder parent.
+// Recursively searches in the subfolders by passing an existing `folders` slice.
+func scanGitFolders(folders []string, folder string) []string {
+	// trim the last `/`
+	folder = strings.TrimSuffix(folder, "/")
+
+	f, err := os.Open(folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+	files, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var path string
+
+	for _, file := range files {
+		if file.IsDir() {
+			path = folder + "/" + file.Name()
+			if file.Name() == ".git" {
+				path = strings.TrimSuffix(path, "/.git")
+				fmt.Println(path)
+				folders = append(folders, path)
+				continue
 			}
-		} else {
-			panic(err)
+			if file.Name() == "vendor" || file.Name() == "node_modules" {
+				continue
+			}
+			folders = scanGitFolders(folders, path)
 		}
 	}
-	return f
+
+	return folders
 }
-
-
-
